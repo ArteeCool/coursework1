f2f4cb01c719feedabfbf4975bc5ebabd57e58b1
ArteeCool <84199800+ArteeCool@users.noreply.github.com>
2025-06-05 10:22:46 +0300

    Changed router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import Layout from "./components/general/Layout";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
@@ -6,18 +6,37 @@ import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import AdminPanel from "./pages/AdminPanel";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/sign-up",
+        element: <SignUp />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+      {
+        path: "/admin-panel",
+        element: <AdminPanel />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/admin-panel" element={<AdminPanel />} />
-      </Route>
-    </Routes>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,10 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router";
 import UserContextProvider from "./context/UserContextProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <UserContextProvider>
-      <App />
-    </UserContextProvider>
-  </BrowserRouter>
+  <UserContextProvider>
+    <App />
+  </UserContextProvider>
 );
